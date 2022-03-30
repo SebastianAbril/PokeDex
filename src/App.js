b916d9db298295bba6a45dca52b76aa7e9f28a58
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-03-30 11:52:15 -0500

    Added Header, Nav. CSS still in work

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {Header} from './Header/index';
+import {Nav} from './Nav/index';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css";
+
+function Header(){
+    return(
+        <header className="Header">
+            <label className="HeaderAbout">About</label>
+            <label className="HeaderTitle">Pokedex</label>
+            <div className="HeaderMode">
+                <label className="HeaderModeDark">Dark</label>
+                <label className="HeaderModeLight">Light</label>
+            </div>
+        </header>
+    )
+}
+
+export {Header};
