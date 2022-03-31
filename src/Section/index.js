bdc07846f2a7312a735cd220692c6661f523439c
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-03-31 08:24:50 -0500

    Errores de importacion modulo SectionCard

--- src/Section/index.js
+++ src/Section/index.js
@@ -6,17 +6,7 @@ import SectionCard from '../SectionCard';
 
 const URL ="https://pokeapi.co/api/v2/";
 
-function Card(){
-    return (
-        <div className='SectionCard'>
-            <label className='SectionCardTitle'>
-                Hawlucha <span className="SectionCardNumber"> #701</span>
-            </label>
-            
-             <img className='SectionCardImage' src="https://assets.nintendo.com/image/upload/ar_4:3,c_pad,dpr_2.0,f_auto,q_auto,w_400/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/egdp/pokemon-2" alt="fenix" />
-        </div>
-    )
-}
+
 
 
 function Section(){
@@ -44,15 +34,15 @@ function Section(){
         
 
     )
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
+            <SectionCard />
             
         </div>
     )
--- src/SectionCard/index.js
+++ src/SectionCard/index.js
@@ -4,10 +4,11 @@ import "./SectionCard.css"
 function SectionCard(){
     return(
         <div className='SectionCard'>
-                <label className='SectionCardTitle'>
-                    Hawlucha <span> #701</span>
-                </label>
-                {/* <img className='SectionCardImage' src="https://assets.nintendo.com/image/upload/ar_4:3,c_pad,dpr_2.0,f_auto,q_auto,w_400/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/egdp/pokemon-2" alt="fenix" /> */}
+            <label className='SectionCardTitle'>
+                Hawlucha <span className="SectionCardNumber"> #701</span>
+            </label>
+            
+             <img className='SectionCardImage' src="https://assets.nintendo.com/image/upload/ar_4:3,c_pad,dpr_2.0,f_auto,q_auto,w_400/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/egdp/pokemon-2" alt="fenix" />
         </div>
     )
 }
