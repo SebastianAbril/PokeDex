8f5cf3ff38353c3f97f5e0c9f5a83741cf4c7541
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-03-30 21:59:24 -0500

    Arreglar funcion Fetch para consumir API, no se encuentran imagenes para renderizar

--- src/Section/index.js
+++ src/Section/index.js
@@ -12,6 +12,7 @@ function Card(){
             <label className='SectionCardTitle'>
                 Hawlucha <span className="SectionCardNumber"> #701</span>
             </label>
+            
              <img className='SectionCardImage' src="https://assets.nintendo.com/image/upload/ar_4:3,c_pad,dpr_2.0,f_auto,q_auto,w_400/v1/ncom/en_US/games/switch/p/pokemon-legends-arceus-switch/egdp/pokemon-2" alt="fenix" />
         </div>
     )
@@ -20,10 +21,29 @@ function Card(){
 
 function Section(){
 
+    const [info, setInfo] = useState({});
 
+    const [pokemon, setPokemon] = useState({});
+
+    const FetchData = () =>{
+        fetch(URL)
+        .then( (response)=> response.json())
+        .then( (data) => setInfo(data) )
+        console.log(info.pokemon);
+
+        fetch(info)
+        .then( (response)=> response.json())
+        .then( (data) => setPokemon(data) )
+        console.log(pokemon)
+    }
+
+    
     return(
         <div className="Section">
-            
+            <button onClick={FetchData}  className="Go">GO!</button>
+        
+
+    )
             <Card />
             <Card />
             <Card />
