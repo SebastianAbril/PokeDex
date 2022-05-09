b3cbf359e7a53a188bf954b39a9e07e712a95fc8
Juan Sebastian Abril <sebastianabril6@gmail.com>
2022-05-09 15:39:46 -0500

    SearchBar: need to be fixed, it is not working

---
+++ src/components/SearchFormat/SearchFormat.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearcFormat.css';
+
+function SearchFormat() {
+  return (
+    <form className="SearchFormat">
+      <label></label>
+      <label></label>
+      <label></label>
+    </form>
+  );
+}
+
+export { SearchFormat };
--- src/screens/PokemonListScreen/PokemonListScreen.jsx
+++ src/screens/PokemonListScreen/PokemonListScreen.jsx
@@ -3,7 +3,8 @@ import { PokemonCard } from '../../components/PokemonCard/PokemonCard';
 import './PokemonListScreen.css';
 import { SearchBar } from '../../components/SearchBar/SearchBar';
 
-const URL = 'https://pokeapi.co/api/v2/pokemon?limit=14';
+const URL = 'https://pokeapi.co/api/v2/pokemon?limit=6';
+/* let NumberOfPokemons; */
 
 export const PokemonListScreen = () => {
   const [paginatedPokemons, setPaginatedPokemons] = useState();
@@ -24,16 +25,25 @@ export const PokemonListScreen = () => {
       />
       <main className="PokemonList">
         {paginatedPokemons &&
-          paginatedPokemons.results.map((pokemon) => {
-            return (
-              <PokemonCard
-                onClick={() => console.log('hola')}
-                key={pokemon.name}
-                name={pokemon.name}
-                url={pokemon.url}
-              />
-            );
-          })}
+          paginatedPokemons.results
+            .filter((pokemon) => {
+              if (searchedPokemon === '') {
+                console.log(paginatedPokemons.results);
+                return pokemon;
+              } else if (pokemon.name.toLowerCase().includes(searchedPokemon.toLowerCase())) {
+                return pokemon;
+              }
+            })
+            .map((pokemon) => {
+              return (
+                <PokemonCard
+                  onClick={() => console.log('hola')}
+                  key={pokemon.name}
+                  name={pokemon.name}
+                  url={pokemon.url}
+                />
+              );
+            })}
       </main>
     </div>
   );
